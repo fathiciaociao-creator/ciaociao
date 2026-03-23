16e256d1f40ee9818bcb90952bcce035a92384bb
Mohammad Alseidi <mfaraj981@gmail.com>
2026-03-23 07:11:48 +0300

    app31

--- src/lib/push.ts
+++ src/lib/push.ts
@@ -1,4 +1,4 @@
-import webpush from 'web-push';
+import * as webpush from 'web-push';
 
 const publicKey = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY;
 const privateKey = process.env.VAPID_PRIVATE_KEY;
