b9d64b1505a821b9e725e4fd3906f80628f3f041
Mohammad Alseidi <mfaraj981@gmail.com>
2026-04-15 22:56:15 +0300

    Ciao Ciao 44

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,12 +5,13 @@ import { PrismaPg } from '@prisma/adapter-pg'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 const connectionString = process.env.DATABASE_URL
-const pool = new Pool({ 
+const pool = new Pool({
   connectionString,
   ssl: {
     rejectUnauthorized: false
   }
 })
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const adapter = new PrismaPg(pool as any)
 
